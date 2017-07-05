beb011070c6f1d52866177120fc2abcf6d616d98
koopero <koopero@gmail.com>
2017-07-05 13:26:50 -0700

    fix bad path

--- script/npm-link.sh
+++ script/npm-link.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-git clone https://github.com/koopero/horten.git
+#
+# Make sure we're in the right directory
+#
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+cd ..
+
 cd horten
 npm install
 npm link
 cd ..
 
-git clone https://github.com/koopero/horten-websocket.git
 cd horten-websocket
 npm install
 npm link horten
 npm link
 cd ..
 
-git clone https://github.com/koopero/deepcolour.git
 cd deepcolour
 npm install
 npm link
 cd ..
 
 
-git clone https://github.com/koopero/horten-control.git
 cd horten-control
 npm install
 npm link horten deepcolour
 npm link
 cd ..
 
-git clone https://github.com/koopero/horten-server.git
 cd horten-server
 npm install
 npm link horten horten-control horten-websocket
 npm link
 cd ..
 
-git clone https://github.com/koopero/loopin-native.git
-cd loopin
+cd loopin-native
 npm install
 npm link
 cd ..
 
-git clone https://github.com/koopero/loopin.git
 cd loopin
 npm install
 npm link horten loopin-native
 npm link
 cd ..
 
-git clone https://github.com/koopero/loopin-server.git
 cd loopin-server
 npm install
 npm link horten loopin horten-server
